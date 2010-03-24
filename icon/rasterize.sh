29a74d808200063042c3362efca9e3f6c7999873
archie.cobbs <archie.cobbs@00468771-1dd6-6e39-6dee-4643f6b6a44a>
2010-03-24 15:30:42 +0000

    - Rasterize 512px image as JPEG - Turn off icon auto-shine

--- icon/rasterize.sh
+++ icon/rasterize.sh
@@ -4,7 +4,9 @@
 BATIK_DIR="${HOME}/batik-1.7"
 RASTERIZER="java -jar ${BATIK_DIR}/batik-rasterizer.jar"
 
-for SIZE in 29 57 512; do
+for SIZE in 29 57; do
     ${RASTERIZER} -m image/png -w ${SIZE} -h ${SIZE} -d OATH_Token${SIZE}.png OATH_Token.svg
 done
+
+${RASTERIZER} -m image/jpeg -q 0.95 -w 512 -h 512 -d OATH_Token512.jpg OATH_Token.svg
 
